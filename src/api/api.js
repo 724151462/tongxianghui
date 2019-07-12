f25f40a39bcfe6cca72c13e898b00dd6f3fd6cd1
吴佳雄 <724151462@qq.com>
2019-07-12 15:44:46 +0800

    修改为线上ip

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,24 @@ import {
   commonsAjax
 } from './request'
 
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'http://255920k78z.qicp.vip'
+  /*第二层if，根据.env文件中的VUE_APP_FLAG判断是生产环境还是测试环境*/
+  // if (process.env.VUE_APP_FLAG === 'prod') {
+  //   //production 生产环境
+  //   axios.defaults.baseURL = 'https://api.tskedu.com';
+  //   window.sockterUrl = 'wss://api.tskedu.com/websocket'
+  // } else {
+  //   //dev 测试环境
+  //   axios.defaults.baseURL = 'http://apidev.tskedu.cn:9008';
+  //   window.sockterUrl = 'ws://apidev.tskedu.cn:9008/websocket'
+  // }
+} else {
+  //本地 开发环境
+  axios.defaults.baseURL = '/api';
+}
+
+
 export function getActList() {
   return commonsAjax(`interaction/v1/page`, 'get')
 }
@@ -19,7 +37,7 @@ export function postAnsform(data) {
 }
 
 export function login(data) {
-  return commonsAjax(`http://255920k78z.qicp.vip/wxx/v1/login`, data, 'post')
+  return commonsAjax(`wxx/v1/login`, data, 'post')
 }
 
 export function getYzm(data) {
