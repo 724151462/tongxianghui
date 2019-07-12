06152731fc1e4115815b024312e0ec25f72f770d
吴佳雄 <724151462@qq.com>
2019-07-12 17:04:58 +0800

    测试

--- src/views/active/index.vue
+++ src/views/active/index.vue
@@ -50,6 +50,7 @@ export default {
       })
     },
     wxScan() {
+      console.log(12345)
       wx.ready(function () {
   // config信息验证成功后会执行ready方法,所有接口调用都必须在config接口获得结果之后
   // config 是一个客户端的异步操作,所以如果需要在页面加载时调用相关接口,则须把相关接口放在ready函数中调用来确保正确执行.对于用户触发是才调用的接口,则可以直接调用,不需要放在ready函数中
@@ -59,6 +60,7 @@ export default {
     ],
     success: function (res) { // 以键值对的形式返回，可用true，不可用false。如：{"checkResult":{"scanQRCode":true},"errMsg":"checkJsApi:ok"}
       if (res.checkResult.scanQRCode === true) {
+        console.log('扫码功能可用')
          wx.scanQRCode({ // 微信扫一扫接口
            desc: 'scanQRCode desc',
            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
